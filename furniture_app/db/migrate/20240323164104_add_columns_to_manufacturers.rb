e5d3b0971b4137c6c6e7f62183be22e0d38ebfac
Bohdan Kurila <kuzmin2021ks11@student.karazin.ua>
2024-03-24 12:58:00 +0200

    Add migrate for manufacturer

---
+++ furniture_app/db/migrate/20240323164104_add_columns_to_manufacturers.rb
@@ -0,0 +1,12 @@
+class AddColumnsToManufacturers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :manufacturers, :person_name, :string, null: false, default: ""
+    add_column :manufacturers, :person_last_name, :string, null: false, default: ""
+    add_column :manufacturers, :person_middle_name, :string
+    add_column :manufacturers, :title_manufacturer, :string, null: false, default: ""
+    add_column :manufacturers, :phone_number, :string, null: false, default: ""
+
+    add_index :manufacturers, :title_manufacturer, unique: true
+    add_index :manufacturers, :phone_number, unique: true
+  end
+end

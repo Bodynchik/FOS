2c893494fed2c91de136b9386b91ab262df6a386
Bohdan Kurila <kuzmin2021ks11@student.karazin.ua>
2024-03-24 13:02:34 +0200

    Update(add routs for manufacturers(registrations, sessions), users profile)

--- furniture_app/config/routes.rb
+++ furniture_app/config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  # devise_for :manufacturers
   # devise_for :users
+
+  #Шлях до реєстрації користувача
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
+  #Шлях до реєстрації виробника
+  devise_for :manufacturers, controllers: {
+    sessions: 'manufacturers/sessions',
+    registrations: 'manufacturers/registrations'
+  }
+
   get 'pages/home'
+
   resources :ordered_products
   resources :carts
   resources :prod_raitings
@@ -18,7 +30,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get '/new', to: 'pages#new'
+  namespace :users do
+    get '/profiles', to: 'profiles#show'
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
